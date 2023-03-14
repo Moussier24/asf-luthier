c201cc1df92c06bd54a2388d69a9c6c2ec6b7478
KevinArmache <kevinarmache@gmail.com>
2023-03-15 00:28:23 +0100

    add: work on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
       {/* MENU */}
       <nav className="  border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div className="container border-b-2 w-90 border-black flex flex-wrap items-center justify-between mx-auto">
-          <div className="mx-auto  md:block md:w-auto" id="navbar-default">
-            <ul className="flex mx-auto  p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li className="border-b-2 border-black p-2 navbar-bg-hover">
+          <div className="mx-auto  md:block w-auto" id="navbar-default">
+            <ul className="flex items-center   justify-evenly mx-auto  p-4 mt-4  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li className="border-b-2 border-black p-2 ">
                 <a
                   href="#"
-                  className="block w-full py-2 pl-3 pr-4  text-black rounded md:bg-transparent  md:text-2xl md:p-0 dark:text-white"
+                  className=" font-assistant block w-full py-2 pl-3 pr-4  text-black rounded md:bg-transparent  md:text-2xl md:p-0 dark:text-white"
                   aria-current="page"
                 >
                   Home
@@ -24,7 +24,7 @@ const Navbar = () => {
               <li className="border-b-2 border-black p-2">
                 <a
                   href="#"
-                  className="block py-2 pl-3 md:text-2xl pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="font-assistant block py-2 pl-3 md:text-2xl pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Instruments
                 </a>
@@ -32,7 +32,7 @@ const Navbar = () => {
               <li className="border-b-2 border-black p-2">
                 <a
                   href="#"
-                  className="block py-2 md:text-2xl pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="font-assistant block py-2 md:text-2xl pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   About
                 </a>
@@ -40,7 +40,7 @@ const Navbar = () => {
               <li className="border-b-2 border-black p-2">
                 <a
                   href="#"
-                  className="block py-2 md:text-2xl pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="font-assistant block py-2 md:text-2xl pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Contact
                 </a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
-import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import Guitar from "../assets/Welcome page.JPG";
 import WebsiteLayout from "@/layouts/WebsiteLayout";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <WebsiteLayout>Salut</WebsiteLayout>
+      <WebsiteLayout>
+        <div className="container-image-home">
+          <Image src={Guitar} alt="Guitar Image" width={500} height={300} />
+        </div>
+      </WebsiteLayout>
     </>
   );
 }
+
+// MD:CSS EST POUR ECRAN LARGE ET PRIS D'ABORD EN PRIORITE
+// CSS DIRECT EST SOIT PRIS EN PRIORITE SI
+// IL YA PAS DE MD: SOIT PRIS EN SECOND LORSQUE IL YA UN MD:
