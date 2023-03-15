716977639cca7e92a7bc5c93f8b96f68295fa5e0
KevinArmache <kevinarmache@gmail.com>
2023-03-15 11:48:05 +0100

    add: work on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,29 @@
-import React from "react";
+import { Icon } from "@iconify/react";
 
 const Footer = () => {
   return (
-    <footer className="bg-transparent border-t-2 border-black rounded-lg shadow dark:bg-gray-900 ml-5 mr-5">
+    <footer className="bg-transparent border-t-2 border-color-secondary  ml-5 mr-5">
       <div className="w-full container mx-auto p-4 md:px-6 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
             href="https://flowbite.com/"
             className="flex items-center mb-4 sm:mb-0"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8 mr-3"
-              alt="Flowbite Logo"
+            <Icon
+              className="block h-10 w-auto lg:hidden"
+              icon="la:guitar"
+              color="#4d4d4d"
+              width="100"
+              height="100"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Asf-luthier
             </span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                About
+                Instruments
               </a>
             </li>
             <li>
@@ -47,7 +49,13 @@ const Footer = () => {
           <a href="https://flowbite.com/" className="hover:underline ">
             <span className="font-bold">asf-luthier.</span>
           </a>
-          &nbsp; All rights reserved. Website developed by Kevin Armache
+          &nbsp; All rights reserved. Website developed by{" "}
+          <a
+            className="border-b-2 hover:text-white border-color-secondary color-bg-secondary-hover px-0.5 "
+            href="https://www.instagram.com/kevinarmache/"
+          >
+            Kevin Armache
+          </a>
         </span>
       </div>
     </footer>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
               <div className="relative flex h-16 items-center justify-between">
                 <div className="selecter absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-color-secondary">
+                  <Disclosure.Button className=" transition-class inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-color-secondary">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XMarkIcon
@@ -94,7 +94,7 @@ const Navbar = () => {
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
+                  <Menu as="div" className="relative ml-3 transition-class">
                     <Transition
                       as={Fragment}
                       enter="transition ease-out duration-100"
@@ -104,14 +104,14 @@ const Navbar = () => {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="transition-class absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
                             <a
                               href="#"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700"
+                                "block px-4 py-2 text-sm transition-class text-gray-700"
                               )}
                             >
                               Your Profile
