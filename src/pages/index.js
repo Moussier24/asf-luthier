f70f2a3dbe53cc7aa0ae26e75b5f8bedab674cad
KevinArmache <kevinarmache@gmail.com>
2023-03-15 16:07:02 +0100

    add: fix error deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Guitar from "../assets/Welcome page.JPG";
+import Guitar from "../assets/guitar.jpg";
 import WebsiteLayout from "@/layouts/WebsiteLayout";
 const inter = Inter({ subsets: ["latin"] });
 
