8c7f8d7690ca806424b9dce1efd668381d8248ac
KevinArmache <kevinarmache@gmail.com>
2023-03-15 19:15:08 +0100

    fix: size texte

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,22 +22,22 @@ const Footer = () => {
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+              <a href="#" className="mr-4 hover:underline md:mr-6  text-xl">
                 Instruments
               </a>
             </li>
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
+              <a href="#" className="mr-4 hover:underline md:mr-6 text-xl">
                 Privacy Policy
               </a>
             </li>
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+              <a href="#" className="mr-4 hover:underline md:mr-6 text-xl">
                 Licensing
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline">
+              <a href="#" className="hover:underline text-xl">
                 Contact
               </a>
             </li>
