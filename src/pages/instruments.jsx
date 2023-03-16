dca42e308fb8a41006df20f78286c6ea5ff2b9e5
KevinArmache <kevinarmache@gmail.com>
2023-03-16 14:57:58 +0100

    add: add card modele

--- src/pages/instruments.jsx
+++ src/pages/instruments.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import WebsiteLayout from "@/layouts/WebsiteLayout";
+import CardInstrument from "@/components/CardInstrument/CardInstrument";
 
 const instruments = () => {
-  return <WebsiteLayout>instruments</WebsiteLayout>;
+  return (
+    <WebsiteLayout>
+      <CardInstrument />
+    </WebsiteLayout>
+  );
 };
 
 export default instruments;
