74d3ce085bc572bcf04802c652b02eb290f397a8
KevinArmache <kevinarmache@gmail.com>
2023-03-17 12:32:37 +0100

    add: models in instrument page

--- src/pages/instruments.jsx
+++ src/pages/instruments.jsx
@@ -5,7 +5,21 @@ import CardInstrument from "@/components/CardInstrument/CardInstrument";
 const instruments = () => {
   return (
     <WebsiteLayout>
-      <CardInstrument />
+      <div className="flex p-1 flex-wrap justify-center border-2 border-black w-80vw">
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        {/*  */}
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+        <CardInstrument />
+      </div>
     </WebsiteLayout>
   );
 };
