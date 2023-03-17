c64bc629c9fde3823af70558407012bdd4d4f5a9
KevinArmache <kevinarmache@gmail.com>
2023-03-17 12:54:48 +0100

    merge with develop

--- src/components/CardInstrument/CardInstrument.jsx
+++ src/components/CardInstrument/CardInstrument.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 const CardInstrument = () => {
   return (
     <div>
-      <div className="w-40 m-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-40 m-1 bg-white border border-gray-200 rounded-lg shadow ">
         <a href="#">
           <Image
             className="rounded-t-lg w-100% mt-5"
