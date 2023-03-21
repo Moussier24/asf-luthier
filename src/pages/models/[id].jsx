3636db582474f4cfcc3123ddc15a79b7408b25f4
KevinArmache <kevinarmache@gmail.com>
2023-03-21 23:20:10 +0100

    fix: link on image instrument

--- src/pages/models/[id].jsx
+++ src/pages/models/[id].jsx
@@ -186,24 +186,24 @@ const id = () => {
         {name}
       </h2>
       <h3 className="text-xl font-bold text-black">Categorie</h3>
-      <span className="text-black">{categorie}</span>
-      <h3 className="text-xl font-bold text-black ">Description</h3>
-      <p className="m-2 md:w-40vw sm:w-70vw text-center text-black ">
+      <span className="text-black text-lg">{categorie}</span>
+      <h3 className="text-lg font-bold text-black ">Description</h3>
+      <p className="m-2 md:w-40vw sm:w-70vw text-lg text-center text-black ">
         {description}
       </p>
       <h3 className="text-xl font-bold text-black">Details</h3>
       <div className="container-details text-center text-black">
-        <span className="text-black"> Nut-width :{width}</span>
+        <span className="text-black text-lg"> Nut-width :{width}</span>
         <br />
-        <span className="text-black"> Scale-length :{length}</span>
+        <span className="text-black text-lg"> Scale-length :{length}</span>
         <br />
-        <span className="text-black"> Weight :{weight}</span>
+        <span className="text-black text-lg"> Weight :{weight}</span>
         <br />
-        <span className="text-black"> Top :{top}</span>
+        <span className="text-black text-lg"> Top :{top}</span>
         <br />
         <br />
         <br />
-        <span className="text-black">
+        <span className="text-black text-lg">
           Pictures Courtesy of {picturesCourtesy}
         </span>
       </div>
